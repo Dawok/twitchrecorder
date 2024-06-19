e787738d2cb63e50dfbe03d7205d6ddbbe9f27a8
Dawok <dawok@proton.me>
2024-06-20 00:25:47 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# User configuration
+username = ""
+quality = "best"
+check_interval = 15
+wait_before_processing = 1800 
+check_interval_while_awaiting_processing = 2
+discord_webhook_url = ""
+
+# Twitch configuration
+client_id = ""
+client_secret = ""
+oauth_token = ""
+
+# Global configuration
+disable_ffmpeg = False
+
+# Folder configuration
+root_path_enabled = False
+root_path = "./downloads"
+#or, if you want to set entirely different folders for unprocessed and processed files
+unprocessed_path = "./recording"
+processed_path = "./processed"
